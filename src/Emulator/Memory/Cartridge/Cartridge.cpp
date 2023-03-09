500f21bce0c4b286246512dbef0357feaf4a9b6a
achansel <107318617+achansel@users.noreply.github.com>
2023-03-09 18:45:13 +0100

    Cartridge: Added small parsing and quick handling of ROMOnly memory controller

--- src/Emulator/Memory/Cartridge/Cartridge.cpp
+++ src/Emulator/Memory/Cartridge/Cartridge.cpp
@@ -5,19 +5,40 @@
 #include <iostream>
 #include "Cartridge.hpp"
 
+// TODO: Fix folder opening
 Cartridge::Cartridge(const std::string& path_to_game) {
     std::ifstream ifs(path_to_game, std::ios::binary | std::ios::ate);
-    if (ifs.fail())
+    if (ifs.fail() || !ifs.good())
     {
         std::cerr << "Error opening file '" << path_to_game << "'" << std::endl;
         exit(1);
     }
-    rom_size = ifs.tellg();
 
-    m_rom = (u8*) malloc(rom_size);
+	/* File loading stuff */
+    m_rom_size = ifs.tellg();
 
+    m_rom = (u8*) malloc(m_rom_size);
     ifs.seekg(0, std::ios::beg);
-    ifs.read((char*) m_rom, rom_size);
+    ifs.read((char*) m_rom, m_rom_size);
+
+	/* Cartridge headers reading */
+	m_title = std::string(m_rom + 0x134, m_rom + 0x143);
+	m_manufacturer_code = std::string(m_rom + 0x13F, m_rom + 0x142);
+	m_cgb_flag = m_rom[0x143];
+	m_cartridge_type = static_cast<CartridgeType>(m_rom[0x147]);
+	m_destination_code = m_rom[0x14A];
+	m_mask_rom_version_number = m_rom[0x14C];
+	//TODO: m_ram_size = m_rom[149];
+
+	std::cout << "GBMU: ROM LOADED:\n";
+	std::cout << "\ttitle: " << m_title << " (manufacturer_code: " << (m_manufacturer_code[0] != '\0' ? m_manufacturer_code : "NONE") << ") version " << std::hex << static_cast<unsigned int>(m_mask_rom_version_number) << "\n";
+	std::cout << "\ttype: " << std::hex << static_cast<unsigned int>(m_cartridge_type) << " \tcgb_flag: " << static_cast<unsigned int>(m_cgb_flag) << "\n";
+	std::cout << "\tdestination: " << std::hex << static_cast<unsigned int>(m_destination_code) << "\n";
+	if (!Cartridge::is_supported_mbc(m_cartridge_type))
+	{
+		std::cout << "GBMU: FATAL: UNSUPPORTED CART TYPE 0X" << std::hex << static_cast<unsigned int>(m_cartridge_type) << "\n";
+		exit(1);
+	}
 }
 
 Cartridge::~Cartridge()
@@ -32,18 +53,29 @@ int Cartridge::get_cart_type()
 
 u8 Cartridge::read_byte(u16 memory_loc) {
     //std::cout << "Cartridge::read_byte -> U16: " << std::hex << memory_loc << std::endl;
-    return memory_loc >= rom_size ? 0 : m_rom[memory_loc];
+    return memory_loc >= m_rom_size ? 0 : m_rom[memory_loc];
 }
 
-u8 Cartridge::read_byte_at_ext_ram(u16 memory_loc) {
-    return m_external_ram[memory_loc];
+u8 Cartridge::read_byte_at_ext_ram(u16 memory_loc)
+{
+	if (m_cartridge_type != CartridgeType::ROM_ONLY)
+		return m_ram[memory_loc];
+	return (0xFF);
 }
 
 void Cartridge::write_byte(u16 memory_loc, u8 value) {
-    std::cout << "Memory Location: " << std::hex << memory_loc << " Value:" << +value << std::endl;
-    m_rom[memory_loc] = value;
+    std::cout << "Cartridge write: memory Location: " << std::hex << memory_loc << " value:" << +value << std::endl;
+    //m_rom[memory_loc] = value;
 }
 
-void Cartridge::write_byte_at_ext_ram(u16 memory_loc, u8 value) {
-    m_external_ram[memory_loc] = value;
+void Cartridge::write_byte_at_ext_ram(u16 memory_loc, u8 value) 
+{
+	if (m_cartridge_type != CartridgeType::ROM_ONLY)
+    	m_ram[memory_loc] = value;
+}
+
+/* Statics */
+bool Cartridge::is_supported_mbc(CartridgeType type)
+{
+	return (type == CartridgeType::ROM_ONLY);
 }
--- src/Emulator/Memory/Cartridge/Cartridge.hpp
+++ src/Emulator/Memory/Cartridge/Cartridge.hpp
@@ -11,6 +11,39 @@
 
 #include "Emulator/Util/Types.hpp"
 
+enum class CartridgeType {
+	ROM_ONLY						= 0x00,
+	/*
+	MBC1							= 0x01,
+	MBC1_RAM						= 0x02,
+	MBC1_RAM_BATTERY				= 0x03,
+	MBC2							= 0x05,
+	MBC2_BATTERY					= 0x06,
+	ROM_RAM							= 0x08,
+	ROM_RAM_BATTERY					= 0x09,
+	MMM_01							= 0x0B,
+	MMM_01_RAM						= 0x0C,
+	MMM_01_RAM_BATTERY				= 0x0D,
+	MBC3_TIMER_BATTERY				= 0x0F,
+	MBC3_TIMER_RAM_BATTERY			= 0x10,
+	MBC3							= 0x11,
+	MBC3_RAM						= 0x12,
+	MBC3_RAM_BATTERY				= 0x13,
+	MBC5							= 0x19,
+	MBC5_RAM						= 0x1A,
+	MBC5_RAM_BATTERY				= 0x1B,
+	MBC5_RUMBLE						= 0x1C,
+	MBC5_RUMBLE_RAM					= 0x1D,
+	MBC5_RUMBLE_RAM_BATTERY			= 0x1E,
+	MBC6							= 0x20,
+	MBC7_SENSOR_RUMBLE_RAM_BATTERY	= 0x22,
+	POCKET_CAMERA					= 0xFC,
+	BANDAI_TAMA5					= 0xFD,
+	HUC3							= 0xFE,
+	HUC1_RAM_BATTERY				= 0xFF,
+	*/
+};
+
 class Cartridge {
 public:
     explicit Cartridge(const std::string& path_to_game);
@@ -23,10 +56,22 @@ public:
 
     void write_byte(u16 memory_loc, u8 value);
     void write_byte_at_ext_ram(u16 memory_loc, u8 value);
+
+	static bool is_supported_mbc(CartridgeType t);
 private:
-	std::array<u8, 8192> m_external_ram{0};
+	std::string		m_title;
+	std::string 	m_manufacturer_code;
+
+	u8				m_cgb_flag;
+	CartridgeType	m_cartridge_type;
+	u8				m_destination_code;
+	u8				m_mask_rom_version_number;
+
+	u8*	m_ram;
+	u32 m_ram_size;
+	
 	u8* m_rom;
-    int rom_size;
+    u32 m_rom_size;
 };
 
 
